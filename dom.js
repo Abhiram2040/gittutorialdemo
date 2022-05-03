4596f32889620fa482719a3819ae2c1b29977803
Abhiram2040 <104695474+Abhiram2040@users.noreply.github.com>
2022-05-04 00:18:22 +0530

    second dom

--- dom.js
+++ dom.js
@@ -26,8 +26,9 @@ headerTitle.style.borderBottom='solid 3px #000'
 //GETELEMENTSBYCLASSNAME//
 var items = document.getElementsByClassName('list-group-item');
 console.log(items);
-console.log(items[1]);
-items[1].textcontent = 'Hello 2';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'green';
+//console.log(items[1]);
+items[2].style.backgroundColor = 'green';
 
+for(var i=0; i<items.length;i++){
+    items[i].style.fontWeight = 'bold';
+}
