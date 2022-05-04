a078224ce2293e494ed4ec665e15980220bd1782
Abhiram2040 <104695474+Abhiram2040@users.noreply.github.com>
2022-05-04 19:52:13 +0530

    task 9

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 
 //Form submit event
 form.addEventListener('submit', addItem);
 //Delete event
 itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
 
 //Add item
 function addItem(e){
@@ -56,4 +59,25 @@ function removeItem(e){
 }
 
 
+//filter Items
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get list
+    var items = itemList.getElementsByTagName('li');
+    // convert to an array
+    Array.from(items).forEach(function(item){
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1){
+            item.style.display = 'block';
 
+        } else{
+            item.style.display = 'none';
+
+        }
+    
+    });
+
+
+
+}
