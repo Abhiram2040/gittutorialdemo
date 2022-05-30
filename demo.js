4b7c47a098ad046e38e07a70fa6ca76bc4e95e42
Abhiram2040 <104695474+Abhiram2040@users.noreply.github.com>
2022-05-30 16:41:44 +0530

    task8 q3

--- demo.js
+++ demo.js
@@ -1,31 +1,10 @@
-const person = {
-
-    fullName: function() {
-  
-      return this.firstName + " " + this.lastName;
-  
-    }
-  
-  }
-  
-  const person1 = {
-  
-    firstName:"John",
-  
-   lastName: "Doe"
-  
-  }
-  
-  const person2 = {
-  
-   firstName:"Mary",
-  
-   lastName: "Doe"
-  
-  }
-  
-  
-  
-  
-  
-  person.fullName.call(person2);
+var geeks = {
+    name : "ABC",
+    printFunc: function(){
+       document.write(this.name);}
+       }
+         
+      var printFunc2= geeks.printFunc.bind(geeks);
+       //using bind() 
+       // bind() takes the object "geeks" as parameter//
+      printFunc2();
